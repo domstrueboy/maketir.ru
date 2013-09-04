29abe95b7d7d447d13f13a8ee7d206c8c69981c0
domstrueboy <domstrueboy@yandex.ru>
2013-09-04 13:57:51 +0500

    Second version
    Добавлено:
    - В маленьком окошке в шапке сайта справа - новости МАКЕТиРа,
    импортированные из публичной страницы вконтакте, с помощью родного
    вконтактовского API;
    - На сайт добавлена галерея lightbox2, использована во второй (3D
    печать) и третьей (Лепнина) вкладках;
    - Изменил логотип на "родной" по просьбе Данила.
    Нужно еще сделать:
    - На первой вкладке (О нас) разместить пару видео в ютуба, дать ссылку
    на канал ютуб;
    - Подобрать для галереи нормальные фотографии и сделать два варианта -
    превью и в хорошем качестве.
    - Что-нибудь еще придумать.

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+//Здесь начинается код кнопки "наверх"
+        var scroll_timer;
+        var displayed = false;
+        var $message = $('#ontop');
+        var $window = $(window);
+        // узнаём позицию блока #top
+        var top = $(document.body).children(0).position().top;
+     
+        // функция при прокрутке
+        $window.scroll(function () {
+            window.clearTimeout(scroll_timer);
+            scroll_timer = window.setTimeout(function() { // используем таймер
+                if($window.scrollTop() <= top+10) /* скрываем, если позиция блока #top и текущий вид окна совпадают.
+                  +10 - для того чтобы показывать кнопку Наверх как минимум при прокрутке окна вниз на 10 пикселей*/
+                {
+                    displayed = false;
+                    $message.fadeOut(300);
+                }
+                else if(displayed == false) // показываем кнопку «Наверх»
+                {
+                    displayed = true;
+                    $message.stop(true, true).fadeTo(300,0.5).click(function () { $message.fadeOut(300); });
+                }
+            }, 100);
+        });
+       
+        // Клик по кнопке наверх
+       $('#ontop').click(function(e) {
+            e.preventDefault();
+           $('body,html').animate({scrollTop: 0}, 300);
+           });
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       VK.Widgets.Group("vk_groups", {mode: 2, wide: 1, width: "auto", height: "auto"}, 39116674);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+                            });
